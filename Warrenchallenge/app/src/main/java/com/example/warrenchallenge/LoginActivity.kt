7c6c92c6a0533f2b582715113d6c5a8e95761376
Marcelo Esser <marcelo@Marcelos-MacBook-Pro.local>
2020-08-20 11:19:14 -0300

    better XML login for performance improvement

--- Warrenchallenge/app/src/main/java/com/example/warrenchallenge/LoginActivity.kt
+++ Warrenchallenge/app/src/main/java/com/example/warrenchallenge/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.warrenchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.VISIBLE
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -12,10 +13,20 @@ class LoginActivity : AppCompatActivity() {
 
         this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
+        lottieControl()
+
+        img_btn_close.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun lottieControl() {
+        lav_money_animation.speed = 1.5f
         lav_money_animation.addAnimatorUpdateListener { animator ->
             val progress = (animator.animatedValue as Float * 100).toInt()
-            if (progress == 80) {
+            if (progress == 30) {
                 lav_money_animation.pauseAnimation()
+                ll_login_fields.visibility = VISIBLE
             }
         }
     }
